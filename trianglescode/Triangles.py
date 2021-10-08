8f24b9c721249da909c10c33b18c6aaecac11b9d
JD Pablo <jdpablo1002@gmail.com>
2021-10-07 20:31:08 -0400

    solved linter issues

--- trianglescode/Triangles.py
+++ trianglescode/Triangles.py
@@ -1,16 +1,17 @@
-import unittest
+"""part of a homework assignment: see function docstring below"""
 import math
 
 
-def classify_triangle(a, b, c):
+def classify_triangle(side_a, side_b, side_c):
+    """inputs 3 values for triangle sides and classifies the type of triangle"""
     triangle_type = ""
-    if a == b == c:
+    if side_a == side_b == side_c:
         triangle_type = "equilateral"
-    elif (a == b != c) or (a != b == c):
+    elif (side_a == side_b != side_c) or (side_a != side_b == side_c):
         triangle_type = "isosceles"
     else:
         triangle_type = "scalene"
-    if a*a + b*b == c*c:
+    if side_a*side_a + side_b*side_b == side_c*side_c:
         triangle_type += " and right"
     return triangle_type
 
